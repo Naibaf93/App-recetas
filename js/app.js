53905efe79dfcdfdb6ce030539cc556dec8018a7
Fabián Huertas <miloh9318@hotmail.com>
2024-11-05 20:20:46 -0500

    mostrando ingredientes y cantidades en el modal

--- js/app.js
+++ js/app.js
@@ -101,6 +101,9 @@ function iniciarApp() {
 
         const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
 
+        console.log(receta);
+        
+
         // Añadir contenido al modal
         const modalTitle = document.querySelector('.modal .modal-title');
         const modalBody = document.querySelector('.modal .modal-body');
@@ -110,7 +113,28 @@ function iniciarApp() {
             <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" /> 
             <h3 class="my-3">Instrucciones</h3>
             <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades</h3>
         `;
+
+        const listGroup = document.createElement('ul');
+        listGroup.classList.add('list-group'); 
+        // Mostrar cantidades e ingredientes
+        for (let i = 1; i <= 20; i++) {
+            if(receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement('li');
+                ingredienteLi.classList.add('list-group-item');
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`
+                
+                listGroup.appendChild(ingredienteLi);
+            }    
+        }
+
+        modalBody.appendChild(listGroup);
+
+
 
         // muestra el modal
         modal.show();
